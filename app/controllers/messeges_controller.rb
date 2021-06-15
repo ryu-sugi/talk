4d54d57679819b5e19154887a93fa205166d251c
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-06-15 12:25:46 +0900

    ルーティング設定

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -1,4 +1,10 @@
 class MessegesController < ApplicationController
   def new
+    @messages = Messege.all
+    @messages = Messege.new
+  end
+
+  def create
+    @message = Messege.new(text: params[:message][:text])
   end
 end
